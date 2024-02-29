8652e3b73d9ff5f6ac37c8a8fe45b2a2350cad8a
Juan Fernando Castro M. <jfcastro9208@gmail.com>
2024-02-29 15:57:46 -0500

    :rocket: CTK-001: Student, Course, Admin entities CRUD structure generated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StudentModule } from './student/student.module';
+import { AdminModule } from './admin/admin.module';
+import { CourseModule } from './course/course.module';
 
 @Module({
-  imports: [],
+  imports: [StudentModule, AdminModule, CourseModule],
   controllers: [AppController],
   providers: [AppService],
 })
